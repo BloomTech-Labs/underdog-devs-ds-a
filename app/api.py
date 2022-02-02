ced16e5b5b0aebeddd61ca51f647e798f37f6433
BrokenShell <webmaster@sharpdesigndigital.com>
2022-02-02 12:55:25 -0800

    adds MongoDB interface and API

--- app/api.py
+++ app/api.py
@@ -1,15 +1,33 @@
 """
 BloomTech Labs DS Machine Learning Operations Role
 - Application Programming Interface
+
+- Project: UnderdogDevs
+- Cluster:
+- Databases:
+    - Mentee
+        - Mentee Application
+        - Mentee Exit Survey (When Mentees graduate)
+    - Mentor
+        - Mentor Application
+        - Mentor Exit Survey (When Mentees graduate)
+        - Mentor Notes
+    - Admin
+        - Admin Notes
 """
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 
+from app.data import MongoDB
+
+
 API = FastAPI(
     title='Underdog Devs DS API',
-    version="0.0.1",
+    version="0.0.2",
     docs_url='/',
 )
+
+API.db = MongoDB("UnderdogDevs")
 
 API.add_middleware(
     CORSMiddleware,
@@ -22,7 +40,34 @@ API.add_middleware(
 
 @API.get("/version")
 async def version():
-    return {"version": API.version}
+    return {"result": API.version}
+
+
+@API.post("/create")
+async def create(collection_name: str, data: dict):
+    """ Creates a new record """
+    return {"result": API.db.create(collection_name, data)}
+
+
+@API.post("/read")
+async def read(collection_name: str, data: dict):
+    """ Returns records based on query """
+    return {"result": API.db.read(collection_name, data)}
+
+
+@API.post("/update")
+async def update(collection_name: str, query: dict, update_data: dict):
+    """ Returns the count of the updated records """
+    old_data = API.db.read(collection_name, query)
+    API.db.update(collection_name, query, update_data)
+    new_data = API.db.read(collection_name, query)
+    return {"result": "What do we return?"}
+
+
+@API.post("/search")
+async def search(collection_name: str, user_search: str):
+    """ Returns all records that match the user_search """
+    return {"result": API.db.search(collection_name, user_search)}
 
 
 if __name__ == '__main__':
--- app/data.py
+++ app/data.py
@@ -2,4 +2,57 @@
 Labs DS Data Engineer Role
 - Database Interface
 - Visualization Interface
+
+
+What to store in MongoDB?
+- Admin Notes
+- Survey Data
 """
+import json
+from os import getenv
+from typing import Optional
+
+from pymongo import MongoClient
+from dotenv import load_dotenv
+
+
+class MongoDB:
+    """ CRUD """
+    load_dotenv()
+
+    def __init__(self, cluster: str):
+        self.cluster = cluster
+
+    def _connect(self, collection: str) -> MongoClient:
+        return MongoClient(getenv("MONGO_URL"))[self.cluster][collection]
+
+    def create(self, collection: str, data: dict) -> dict:
+        self._connect(collection).insert_one(dict(data))
+        return data
+
+    def read(self, collection: str, query: Optional[dict] = None) -> list[dict]:
+        return list(self._connect(collection).find(query, {"_id": False}))
+
+    def update(self, collection: str, query: dict, update_data: dict) -> int:
+        n_changed_records = self.count(collection, query)
+        self._connect(collection).update_many(query, {"$set": update_data})
+        return n_changed_records
+
+    def delete(self, collection: str, query: dict):
+        self._connect(collection).delete_many(query)
+
+    def count(self, collection: str, query: dict) -> int:
+        return self._connect(collection).count_documents(query)
+
+    def backup(self, collection: str):
+        with open("data.json", "w") as file:
+            json.dump(self.read(collection), file)
+
+    def create_index(self, collection: str):
+        self._connect(collection).create_index([("$**", "text")])
+
+    def drop_index(self, collection: str):
+        self._connect(collection).drop_index([("$**", "text")])
+
+    def search(self, collection: str, user_search: str) -> list[dict]:
+        return self.read(collection, {"$text": {"$search": user_search}})
