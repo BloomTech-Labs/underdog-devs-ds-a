bafea44acbd7be3f21f1bae6bf63af62cf51b2be
Evgeny Khoroshukhin <horoshuhin@gmail.com>
2022-02-15 18:21:19 -0500

    latest

--- test_coverage/api_testing.py
+++ test_coverage/api_testing.py
@@ -1,41 +1,55 @@
-import requests
-import doctest
-from app.api import version, scan_collections, create, read, update, search
-from app.api import API
-
-
-version_link='http://underdog-devs-ds-a-dev.us-east-1.elasticbeanstalk.com/version'
 
 
 
 
+def test_coverage():
 
-def test_coverage(version, scan_collections, create, read, update, search):
     '''
-    >>>requests.get(version_link)
+    >>> from app.api import version, scan_collections, create, read, update, search
+    >>> from app.api import API
+    >>> import requests
+    >>> version_link = 'http://underdog-devs-ds-a-dev.us-east-1.elasticbeanstalk.com/version'
+    >>> scan_collections_link = 'http://underdog-devs-ds-a-dev.us-east-1.elasticbeanstalk.com/scan_collections'
+    >>> create_link = 'http://underdog-devs-ds-a-dev.us-east-1.elasticbeanstalk.com/create'
+    
+    >>> read_link = 'http://underdog-devs-ds-a-dev.us-east-1.elasticbeanstalk.com/read'
+    >>> update_link = 'http://underdog-devs-ds-a-dev.us-east-1.elasticbeanstalk.com/update'
+    >>> search_link = 'http://underdog-devs-ds-a-dev.us-east-1.elasticbeanstalk.com/search'
+    >>> requests.get(version_link)
+    
     <Response [200]>
-    >>>requests.get(version_link).json()==version()
+    
+    >>> requests.get(version_link).json()==version()
+    True
+
+    >>> requests.get(scan_collections_link)
+    <Response [200]>
+    >>> requests.get(scan_collections_link).json()==scan_collections()
     True
     '''
-    scan_collections({"result": API.db.scan_collections()})=={}
-    """ Self scans and returns names of collections along with thier size """
+test_coverage()
 
-    create({"result": API.db.create(collection_name, data)})
-    """ Creates a new record 
-        (collection_name: str, data: Dict)"""
+if __name__ == "__main__":
 
-    read({"result": API.db.read(collection_name, data)})
-    """ Returns records based on query
-        (collection_name: str, data: Dict)"""
-
-    update(collection_name: str, query: Dict, update_data: Dict):
-    """ Returns the count of the updated records """
-
-    {"result": (query, update_data)}
-
-    search(collection_name: str, user_search: str):
-        """ Returns all records that match the user_search """
-    {"result": API.db.search(collection_name, user_search)}
+    import doctest
+    doctest.testmod()
 
 
-test_coverage(version, scan_collections, create, read, update, search)
+    # create({"result": API.db.create(collection_name, data)})
+    # """ Creates a new record
+    #     (collection_name: str, data: Dict)"""
+
+    # read({"result": API.db.read(collection_name, data)})
+    # """ Returns records based on query
+    #     (collection_name: str, data: Dict)"""
+
+    # update(collection_name: str, query: Dict, update_data: Dict):
+    # """ Returns the count of the updated records """
+
+    # {"result": (query, update_data)}
+
+
+    # search(collection_name: str, user_search: str):
+    #     """ Returns all records that match the user_search """
+    # {"result": API.db.search(collection_name, user_search)}
+    # test_coverage(version, scan_collections, create, read, update, search)
