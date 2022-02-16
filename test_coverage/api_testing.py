9d831337b35c55925b7d1fa487b11eb3052a6198
Michael Tran <mirayami.mt@gmail.com>
2022-02-16 10:27:01 -0600

    fixed testmod function call

--- test_coverage/api_testing.py
+++ test_coverage/api_testing.py
@@ -19,4 +19,4 @@ if __name__ == "__main__":
     read_link = 'http://underdog-devs-ds-a-dev.us-east-1.elasticbeanstalk.com/read'
     update_link = 'http://underdog-devs-ds-a-dev.us-east-1.elasticbeanstalk.com/update'
     search_link = 'http://underdog-devs-ds-a-dev.us-east-1.elasticbeanstalk.com/search'
-    doctest.testmod(name='test', verbose=True)
+    doctest.testmod(name='test_api', verbose=True)
