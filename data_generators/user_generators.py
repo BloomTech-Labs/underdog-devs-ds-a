ebdde9511e0c39c82c57e34309dd4d66e16f75bf
chunyanwang <christine.cyan.wang@gmail.com>
2022-02-16 15:43:50 -0800

    Bug fix - intermittent bug in connection script for Mongo

--- data_generators/user_generators.py
+++ data_generators/user_generators.py
@@ -23,49 +23,31 @@ class Mentor:
         self.subject = choice(subjects)
         self.skill_level = choice(skill_levels)
 
-class MenteeFeedback:
+
+class Feedback:
 
     def __init__(self, mentees_ids, mentors_ids):
         self.mentee_id = choice(mentees_ids)
         self.mentor_id = choice(mentors_ids)
-        self.ticket_id = int(str(self.mentee_id) + str(self.mentor_id))
+        self.ticket_id = randint(1000000, 9000000)
         self.feedback = choice(feedbacks)
-
-def create_many(collection, number_records):
-
-    if collection == 'Mentees':
-        try:
-            db._connect(collection).drop()
-            db._connect(collection).create_index("user_id", unique=True)
-            db.create_many(collection, (vars(Mentee()) for _ in range(number_records)))
-        except:
-            create_many(collection)
-
-    if collection == 'Mentors':
-        try:
-            db._connect(collection).drop()
-            db._connect(collection).create_index("user_id", unique=True)
-            db.create_many(collection, (vars(Mentor()) for _ in range(number_records)))
-        except:
-            create_many(collection)
-
-    if collection == 'Feedback':
-        try:
-            db._connect(collection).drop()
-            db._connect(collection).create_index("ticket_id", unique=True)
-            db.create_many(collection,
-                           (vars(MenteeFeedback([m['user_id'] for m in mentees],
-                                                [m['user_id'] for m in mentors]))
-                            for _ in range(number_records)))
-        except:
-            create_many(collection)
 
 if __name__ == '__main__':
 
     # codes from github
     db = MongoDB("UnderdogDevs")
-    create_many("Mentees", 100)
-    create_many("Mentors", 20)
+    db._connect("Mentees").drop()
+    db.reset_collection("Mentees")
+    db.create_many("Mentees", [vars(Mentee()) for _ in range(100)])
+    db._connect("Mentors").drop()
+    db.reset_collection("Mentors")
+    db.create_many("Mentors", [vars(Mentor()) for _ in range(20)])
     mentees = db.read("Mentees")
     mentors = db.read("Mentors")
-    create_many("Feedback", 100)
+    db._connect("Feedbacks").drop()
+    db.reset_collection("Feedbacks")
+    feedbacks = [vars(Feedback([m['user_id'] for m in mentees],
+                             [m['user_id'] for m in mentors]))
+               for i in range(200)]
+    db.create_many("Feedbacks", feedbacks)
+
