036eab305413d766bfcc6b2fe67c3b4d983a5817
Michael Tran <mirayami.mt@gmail.com>
2022-02-15 20:37:58 -0600

    fixed doctest

--- test_coverage/api_testing.py
+++ test_coverage/api_testing.py
@@ -1,22 +1,22 @@
+'''
+    >>> str(requests.get(version_link))=='<Response [200]>'
+    True
+    >>> requests.get(version_link).json()=={'result': '0.0.2'}
+    True
 
 '''
->>> str(requests.get(version_link))=='<Response [200]>'
-True
->>> requests.get(version_link).json()=={'result': '0.0.2'}
-True
 
-'''
+from unicodedata import name
+
 
 if __name__ == "__main__":
+    import doctest
+    import requests
+    
     version_link = 'http://underdog-devs-ds-a-dev.us-east-1.elasticbeanstalk.com/version'
     scan_collections_link = 'http://underdog-devs-ds-a-dev.us-east-1.elasticbeanstalk.com/scan_collections'
     create_link = 'http://underdog-devs-ds-a-dev.us-east-1.elasticbeanstalk.com/create'
     read_link = 'http://underdog-devs-ds-a-dev.us-east-1.elasticbeanstalk.com/read'
     update_link = 'http://underdog-devs-ds-a-dev.us-east-1.elasticbeanstalk.com/update'
     search_link = 'http://underdog-devs-ds-a-dev.us-east-1.elasticbeanstalk.com/search'
-    import requests
-    from app.api import API
-    from app.api import version, collections
-
-    import doctest
-    doctest.testmod()
+    doctest.testmod(name='test', verbose=True)
