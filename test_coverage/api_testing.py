29990624bb2333e44d13301165619d3b8e3c7b1f
Michael Tran <mirayami.mt@gmail.com>
2022-02-16 10:11:40 -0600

    working api_testing!

--- test_coverage/api_testing.py
+++ test_coverage/api_testing.py
@@ -1,14 +1,15 @@
-'''
-    >>> requests.get(version_link)
-    <Response [200]>
-    >>> requests.get(version_link).json()
-    {'result': '0.0.2'}
-
-'''
+def test():
+    '''
+        >>> requests.get(version_link)
+        <Response [200]>
+        >>> requests.get(version_link).json()
+        {'result': '0.0.2'}
+    '''
 
 if __name__ == "__main__":
     import doctest
     import requests
+    from app import api
 
     version_link = 'http://underdog-devs-ds-a-dev.us-east-1.elasticbeanstalk.com/version'
     scan_collections_link = 'http://underdog-devs-ds-a-dev.us-east-1.elasticbeanstalk.com/scan_collections'
