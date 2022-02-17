5978eaac3e37293aeb0fc440af9ab2e5b52ff59b
chunyanwang <christine.cyan.wang@gmail.com>
2022-02-16 16:11:49 -0800

    Create a new Collection Feedbacks in MongoDB Database UnderdogDevs, which stores modck data "categorical mentee Feedbacks".

--- data_generators/user_generators.py
+++ data_generators/user_generators.py
@@ -24,30 +24,28 @@ class Mentor:
         self.skill_level = choice(skill_levels)
 
 
-class Feedback:
+class MenteeFeedback:
 
-    def __init__(self, mentees_ids, mentors_ids):
-        self.mentee_id = choice(mentees_ids)
-        self.mentor_id = choice(mentors_ids)
+    def __init__(self, mentee_ids, mentor_ids):
         self.ticket_id = randint(1000000, 9000000)
+        self.mentee_id = choice(mentee_ids)
+        self.mentor_id = choice(mentor_ids)
         self.feedback = choice(feedbacks)
 
-if __name__ == '__main__':
 
-    # codes from github
+if __name__ == '__main__':
     db = MongoDB("UnderdogDevs")
-    db._connect("Mentees").drop()
+
     db.reset_collection("Mentees")
-    db.create_many("Mentees", [vars(Mentee()) for _ in range(100)])
-    db._connect("Mentors").drop()
+    db.create_many("Mentees", (vars(Mentee()) for _ in range(100)))
+
     db.reset_collection("Mentors")
-    db.create_many("Mentors", [vars(Mentor()) for _ in range(20)])
+    db.create_many("Mentors", (vars(Mentor()) for _ in range(20)))
+
+    db.reset_collection("Feedbacks")
     mentees = db.read("Mentees")
     mentors = db.read("Mentors")
-    db._connect("Feedbacks").drop()
-    db.reset_collection("Feedbacks")
-    feedbacks = [vars(Feedback([m['user_id'] for m in mentees],
-                             [m['user_id'] for m in mentors]))
-               for i in range(200)]
+    feedbacks = [vars(MenteeFeedback([m['user_id'] for m in mentees],
+                                     [m['user_id'] for m in mentors]))
+                 for _ in range(200)]
     db.create_many("Feedbacks", feedbacks)
-
