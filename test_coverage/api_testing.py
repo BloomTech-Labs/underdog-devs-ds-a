40096ba8b6b6c1f9f29c4923d5caf3257a7c51b3
Michael Tran <mirayami.mt@gmail.com>
2022-02-16 18:17:31 -0600

    updated w/FastAPI testing

--- test_coverage/api_testing.py
+++ test_coverage/api_testing.py
@@ -1,30 +1,48 @@
-def test_api():
-    '''
-        >>> requests.get(version_link)
+import doctest
+from typing import Dict, Optional
+from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+
+from app.api import API
+from fastapi.testclient import TestClient
+
+client = TestClient(API)
+
+def test_version():
+    """ Returns the current version of the API. 
+        >>> client.get('/version')
         <Response [200]>
-        >>> requests.get(version_link).json()
-        {'result': '0.0.2'}
-        
-        >>> requests.get(create_link).json()
-        
-        >>> requests.get(delete).json()
-        
-        
-        
-        
-        
-    '''
+        >>> client.get('/version').json()
+        {'result': '0.0.3'}
+    """
+
+def test_collections():
+    """ Returns collection name and size of each collection. 
+        >>> client.get('/collections')
+        <Response [200]>
+        >>> type(client.get('/collections').json())
+        <class 'dict'>
+    """
+
+def test_create_read_delete(collection: str, data: Dict):
+    """ Creates a new record.
+        >>> client.get('/test/create')
+        <Response [200]>
+        >>> client.get('/test/read').json()
+
+        >>> client.get('test/delete')
+    """
+
+def test_update(collection: str, query: Dict, update_data: Dict):
+    """ Returns an array containing the query and updated data. """
+
+def test_search(collection: str, user_search: str):
+    """ Returns array of records that loosely match the search,
+    automatically ordered by relevance. """
+
+def test_match(mentee_id: int, n_matches: int):
+    """ Returns array of mentor matches for any given mentee_id. """
+
 
 if __name__ == "__main__":
-    import doctest
-    import requests
-    from app.api import API,read,create,delete
-
-    version_link = 'http://underdog-devs-ds-a-dev.us-east-1.elasticbeanstalk.com/version'
-    scan_collections_link = 'http://underdog-devs-ds-a-dev.us-east-1.elasticbeanstalk.com/scan_collections'
-    create_link = 'http://underdog-devs-ds-a-dev.us-east-1.elasticbeanstalk.com/testing/create'
-    read_link = 'http://underdog-devs-ds-a-dev.us-east-1.elasticbeanstalk.com/read'
-    update_link = 'http://underdog-devs-ds-a-dev.us-east-1.elasticbeanstalk.com/update'
-    search_link = 'http://underdog-devs-ds-a-dev.us-east-1.elasticbeanstalk.com/search'
-    delete_link = 'http://underdog-devs-ds-a-dev.us-east-1.elasticbeanstalk.com/testing/delete'
-    doctest.testmod(name='test_api', verbose=True)
+    doctest.testmod(verbose=True)
