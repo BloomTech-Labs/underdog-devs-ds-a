31af4fa9f3976b55952eeeaeb05b6c7929637ae5
Evgeny Khoroshukhin <horoshuhin@gmail.com>
2022-02-16 21:54:26 -0500

    add assertion testing

--- test_coverage/api_testing.py
+++ test_coverage/api_testing.py
@@ -20,28 +20,27 @@ def test_collections():
     """ Returns collection name and size of each collection. 
         >>> client.get('/collections')
         <Response [200]>
-        >>> type(client.get('/collections').json())
-        <class 'dict'>
+        >>> print(client.get('/collections').json())
     """
 
-def test_create_read_delete(collection: str, data: Dict):
-    """ Creates a new record.
-        >>> client.get('/test/create')
-        <Response [200]>
-        >>> client.get('/test/read').json()
+# def test_create_read_delete(collection: str, data: Dict):
+#     """ Creates a new record.
+#         >>> client.get('/test/create')
+#         <Response [200]>
+#         >>> client.get('/test/read').json()
 
-        >>> client.get('test/delete')
-    """
+#         >>> client.get('test/delete')
+#     """
 
-def test_update(collection: str, query: Dict, update_data: Dict):
-    """ Returns an array containing the query and updated data. """
+# def test_update(collection: str, query: Dict, update_data: Dict):
+#     """ Returns an array containing the query and updated data. """
 
-def test_search(collection: str, user_search: str):
-    """ Returns array of records that loosely match the search,
-    automatically ordered by relevance. """
+# def test_search(collection: str, user_search: str):
+#     """ Returns array of records that loosely match the search,
+#     automatically ordered by relevance. """
 
-def test_match(mentee_id: int, n_matches: int):
-    """ Returns array of mentor matches for any given mentee_id. """
+# def test_match(mentee_id: int, n_matches: int):
+#     """ Returns array of mentor matches for any given mentee_id. """
 
 
 if __name__ == "__main__":
