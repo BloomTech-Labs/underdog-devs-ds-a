b450b2a82a4fa12568ab7813f3538f900e592a56
BrokenShell <webmaster@sharpdesigndigital.com>
2022-02-18 12:15:28 -0800

    refactors Vader to be slightly more Pythonic

--- app/vader_sentiment.py
+++ app/vader_sentiment.py
@@ -1,8 +1,31 @@
+from typing import Callable
+
 from vaderSentiment.vaderSentiment import SentimentIntensityAnalyzer
 
-vader = SentimentIntensityAnalyzer()
 
-# compound score of the sentiment
-def vader_score(text: list) -> list:
+class SentimentAnalyzer:
+    analyzer = SentimentIntensityAnalyzer()
 
-    return [vader.polarity_scores(t)["compound"] for t in text]
+    def __call__(self, text: str) -> float:
+        return self.analyzer.polarity_scores(text)["compound"]
+
+
+def sentiment_analysis() -> Callable:
+    analyzer = SentimentIntensityAnalyzer()
+
+    def worker(text) -> float:
+        return analyzer.polarity_scores(text)["compound"]
+
+    return worker
+
+
+sa_class = SentimentAnalyzer()
+print(sa_class("Some text"))
+
+sa_func = sentiment_analysis()
+print(sa_func("Some text"))
+
+
+positive = "👍"
+neutral = "👌"
+negative = "👎"
