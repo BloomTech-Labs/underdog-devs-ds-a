87f3b78df5b2802f111b6abb210562fcca5e4397
Evgeny Khoroshukhin <horoshuhin@gmail.com>
2022-02-17 22:36:52 -0500

    adds fastapi test

--- test_coverage/fastapi_testing.py
+++ test_coverage/fastapi_testing.py
@@ -1,13 +1,8 @@
 from fastapi.testclient import TestClient
-from fastapi.middleware.cors import CORSMiddleware
-
-from app.data import MongoDB
-
 from app.api import API
 
 client = TestClient(API)
 
-API.db = MongoDB("UnderdogDevs")
 
 def test_version():
     response = client.get("/version")
@@ -22,14 +17,15 @@ def test_collections():
     # assert response.status_code == 200
     # assert response.json() == {'result': '0.0.3'}
 
-def test_create():
-    response = client.get("/test/create/")
-    
+
+def test_read():
+    response = client.post("/Mentees/read", json={'first_name': 'Sage'})
     print(response.json())
     # assert response.status_code == 200
+    # assert response.json() == {'result': '0.0.3'}
 
 
 if __name__ == '__main__':
     # test_version()
-    test_create()
-    test_collections()
+    # test_collections()
+    test_read()
