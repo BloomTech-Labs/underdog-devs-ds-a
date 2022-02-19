d399c2280faeb5e58c4ddbf532aed89cd4234e25
Evgeny Khoroshukhin <horoshuhin@gmail.com>
2022-02-18 19:33:40 -0500

    adds create and delete tests

--- test_coverage/fastapi_testing.py
+++ test_coverage/fastapi_testing.py
@@ -1,9 +1,11 @@
+from typing import Collection
 from black import json
 from fastapi.testclient import TestClient
 from app.api import API
 import sys
 client = TestClient(API)
 sys.tracebacklimit = 0
+
 
 def test_version():
     response = client.get("/version")
@@ -15,8 +17,7 @@ def test_version():
 def test_collections():
     response = client.get("/collections")
     print(response.json())
-    # assert response.status_code == 200
-    # assert response.json() == {'result': '0.0.3'}
+    assert response.status_code == 200
 
 
 def test_read():
@@ -26,18 +27,38 @@ def test_read():
     assert response.json()['result'][0]['first_name'] == 'Sage'
 
 
-def test_create():
-    response = client.post(
-        '/Mentees/create', json={'profile_id': 'TEST', 'first_name': 'Jordan', 'last_name': 'Peele'})
-    read = client.post('/Mentees/read', json={'first_name': 'Jordan'})
-    print(response.json()['result'][0]['first_name'] == 'Jordan')
+def test_create_delete():
+    response = client.post('/Mentees/create', data='''{
+        "profile_id": "test001",
+        "first_name": "Luca",
+        "last_name": "Evans",
+        "email": "fake@email.com",
+        "city": "Ashland",
+        "state": "Oregon",
+        "country": "USA",
+        "formerly_incarcerated": true,
+        "underrepresented_group": true,
+        "low_income": true,
+        "list_convictions": [
+          "Infraction",
+          "Felony"
+        ],
+        "subject": "Web: HTML, CSS, JavaScript",
+        "experience_level": "Beginner",
+        "job_help": false,
+        "industry_knowledge": false,
+        "pair_programming": true,
+        "other_info": "Notes"
+    }''')
 
-
-def test_delete():
-    response = client.post("/Mentees/delete", json={'profile_id': 'TEST'})
+    assert response.json()['result']['profile_id'] == 'test001', 'ERROR'
+    response = client.delete('/Mentees/delete/test001')
+    read = client.post("/Mentees/read", json={'profile_id': 'test001'})
+    assert read.json()['result'] == [], 'ERROR'
 
 
 if __name__ == '__main__':
-    # test_version()
-    # test_collections()
+    test_version()
     test_read()
+    test_create_delete()
+    test_collections()
