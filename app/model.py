ccf80a4fba09e63f9d25bbfb306fb9e2587c1def
RylandGomez <rylandalexandergomez@gmail.com>
2022-02-25 10:57:58 -0600

    Refactored callable class docstrings
    Moved __call__ detailed documentation to the class docstring

--- app/model.py
+++ app/model.py
@@ -6,23 +6,24 @@ from app.utilities import dict_to_str
 
 
 class MatcherSort:
-    """Matching class implemeting simple sorting algorithm."""
+    """Callable matching class implemeting simple sorting algorithm.
+    
+     
+    Organizes mentors based on mentee's subject of interest and
+    their self-defined skill level, corresponding to mentors'
+    teachable subjects and desired skill level(s) to teach.
+
+    Args:
+        n_matches (int): Number of mentor matches desired
+        mentee_id (int): ID number of mentee that needs mentor
+    
+    Returns:
+        List of mentor IDs
+    """
     db = MongoDB("UnderdogDevs")
 
     def __call__(self, n_matches: int, mentee_id: int) -> List[int]:
-        """Return a list of matched mentor id numbers.
-        
-        Organizes mentors based on mentee's subject of interest and
-        their self-defined skill level, corresponding to mentors'
-        teachable subjects and desired skill level(s) to teach.
-    
-        Args:
-            n_matches (int): Number of mentor matches desired
-            mentee_id (int): ID number of mentee that needs mentor
-        
-        Returns:
-            List of mentor IDs
-        """
+        """Return a list of matched mentor id numbers."""
         mentee = self.db.first("Mentees", {"user_id": mentee_id})
 
         def sort_mentors(mentor: Dict) -> Tuple:
@@ -37,48 +38,48 @@ class MatcherSort:
 
 
 class MatcherSearch:
-    """Matching class implementing simple searching algorithm."""
+    """Callable matching class implementing simple searching algorithm.
+        
+    Searches for mentors in database that match the given mentee's
+    subject. Note that n_matches is a maximum for the number of
+    mentors returned, not a guaranteed count.
+
+    Args:
+        n_matches (int): Number of mentor matches desired
+        mentee_id (int): ID number of mentee that needs mentor
+    
+    Returns:
+        List of mentor IDs
+    """
     db = MongoDB("UnderdogDevs")
 
     def __call__(self, n_matches: int, mentee_id: int) -> List[int]:
-        """Return a list of matched mentor id numbers.
-        
-        Searches for mentors in database that match the given mentee's
-        subject. Note that n_matches is a maximum for the number of
-        mentors returned, not a guaranteed count.
-    
-        Args:
-            n_matches (int): Number of mentor matches desired
-            mentee_id (int): ID number of mentee that needs mentor
-        
-        Returns:
-            List of mentor IDs
-        """
+        """Return a list of matched mentor id numbers."""
         mentee = self.db.first("Mentees", {"user_id": mentee_id})
         results = self.db.search("Mentors", mentee["subject"])[:n_matches]
         return [mentor["user_id"] for mentor in results]
 
 
 class MatcherSortSearch:
-    """Matching class implementing simple sort-search algorithm."""
+    """Callable matching class implementing simple sort-search algorithm.
+        
+    Searches for mentors in database that match the given mentee's
+    subject, and then organizes subsequent mentors based on
+    mentee's subject of interest and their self-defined skill
+    level, corresponding to mentors' teachable subjects and desired
+    skill level(s) to teach.
+
+    Args:
+        n_matches (int): Number of mentor matches desired
+        mentee_id (int): ID number of mentee that needs mentor
+    
+    Returns:
+        List of mentor IDs
+    """
     db = MongoDB("UnderdogDevs")
 
     def __call__(self, n_matches: int, mentee_id: int) -> List[int]:
-        """Return a list of matched mentor id numbers.
-        
-        Searches for mentors in database that match the given mentee's
-        subject, and then organizes subsequent mentors based on
-        mentee's subject of interest and their self-defined skill
-        level, corresponding to mentors' teachable subjects and desired
-        skill level(s) to teach.
-    
-        Args:
-            n_matches (int): Number of mentor matches desired
-            mentee_id (int): ID number of mentee that needs mentor
-        
-        Returns:
-            List of mentor IDs
-        """
+        """Return a list of matched mentor id numbers."""
         mentee = self.db.first("Mentees", {"user_id": mentee_id})
 
         def sort_mentors(mentor: Dict) -> Tuple:
@@ -95,23 +96,23 @@ class MatcherSortSearch:
 
 
 class MatcherRandom:
-    """Matching class returning random matches."""
+    """Callable matching class returning random matches.
+        
+    Randomly samples the mentor database for n_matches number of
+    mentors. Note that this function ignores mentee data, and the
+    matches are simply random samples of mentors.
+
+    Args:
+        n_matches (int): Number of mentor matches desired
+        mentee_id (int) (unused): ID number of mentee needing match
+    
+    Returns:
+        List of mentor IDs
+    """
     db = MongoDB("UnderdogDevs")
 
     def __call__(self, n_matches: int, mentee_id: int) -> List[int]:
-        """Return a list of matched mentor id numbers.
-        
-        Randomly samples the mentor database for n_matches number of
-        mentors. Note that this function ignores mentee data, and the
-        matches are simply random samples of mentors.
-    
-        Args:
-            n_matches (int): Number of mentor matches desired
-            mentee_id (int) (unused): ID number of mentee needing match
-        
-        Returns:
-            List of mentor IDs
-        """
+        """Return a list of matched mentor id numbers."""
         results = sample(self.db.read("Mentors"), k=n_matches)
         return [mentor["user_id"] for mentor in results]
 
