26d495e853f05d379a8d6555ae7e63c2ead2f96b
Michael Tran <mirayami.mt@gmail.com>
2022-03-01 18:09:48 -0600

    added integration testing & population db options

--- test_coverage/api_testing.py
+++ test_coverage/api_testing.py
@@ -1,103 +1,76 @@
+from cgi import test
+import json
 import sys
 from fastapi.testclient import TestClient
-from app.data import MongoDB
 from app.api import API
+from data_generators.user_generators import Mentee, Mentor
+from pprint import pprint
 
 
 class TestingPlatform():
     sys.tracebacklimit = 0
     client = TestClient(API)
-    API.db = MongoDB("UnderdogDevs")
-
+    repopulate = False
     def test_version(self):
         """ Tests the current version of the API. """
 
         response = self.client.get("/version")
 
         assert response.status_code == 200, \
-            "Expected Code 200. Got " + str(response.status_code)
+            pprint("test_version: Expected Code 200. Got " +
+                   str(response.status_code))
 
         assert response.json() == {'result': '0.43.7'}, \
-            "Expected {'result': '0.43.7'} got " + str(response.json())
+            pprint("test_version: Expected 0.43.7. Got " + str(response.json()))
 
-    def test_read(self):
+    def test_read(self, query=None):
         """ Tests whether or not a collection can be read in"""
 
         response = self.client.post(
-            "/Mentees/read", json={'first_name': 'Sage'})
+            "/Mentees/read", json=query or None)
 
         assert response.status_code == 200, \
-            "Expected Code 200. Got " + str(response.status_code)
-    
+            pprint("test_read: Expected Code 200. Got " +
+                   str(response.status_code))
+
+        assert response.json() != {}, \
+            pprint("test_read: Expected non empty json. Got " +
+                   str(response.json()))
+
     def test_collections(self):
         """ Tests collection name and size of each collection. """
 
         response = self.client.get("/collections")
 
         assert response.status_code == 200, \
-            "Expected Code 200. Got " + str(response.status_code)
+            pprint("test_collections: Expected Code 200. Got " +
+                   str(response.status_code))
 
         assert response.json() != {}, \
-            "Expected non empty JSON. Got " + str(response.json())
+            "test_collections: Expected non empty JSON. Got " + \
+            str(response.json())
 
-    def test_create_delete(self):
-        """ Creates a new record.
-        Input Example:
-        collection = "Mentees"
-        data = {
-            "profile_id": "test001",
-            "first_name": "Luca",
-            "last_name": "Evans",
-            "email": "fake@email.com",
-            "city": "Ashland",
-            "state": "Oregon",
-            "country": "USA",
-            "formerly_incarcerated": true,
-            "underrepresented_group": true,
-            "low_income": true,
-            "list_convictions": [
-            "Infraction",
-            "Felony"
-            ],
-            "subject": "Web: HTML, CSS, JavaScript",
-            "experience_level": "Beginner",
-            "job_help": false,
-            "industry_knowledge": false,
-            "pair_programming": true,
-            "other_info": "Notes"
-        }
-
-        And checks if collection gets Deleted 
-    """
-
-        response = self.client.get("/test/create/")
-
-        assert response.status_code == 200, \
-            "Expected Code 200. Got " + str(response.status_code)
-    
-    def test_update(self):
+    def test_update(self, query):
+        # Run after create
         """ Returns an array containing the query and updated data. """
+        update = self.client.post("/Mentees/update",
+                                  json={"query": {"profile_id": "test001"},
+                                        "update_data": {"city": "Glorious", "state": "Homeland"}})
+        read = self.client.post(
+            "/Mentees/read", json={"profile_id": "test001"})
 
-        profile_id = 'oW2N3A218zp83Q0i'
-        response = self.client.post("/Mentees/update", json={"query": {"profile_id": profile_id},
-            "update_data": {"first_name": "John"}})
+        assert update.json() == read.json(), "test_update: Expected the update to equal " + str(read.json()) \
+            + ". Got " + str(update.json())
 
-        read_mentees = self.client.post(
-            "/Mentees/read", json={'profile_id': profile_id})
-
-        assert read_mentees.json()['result'][0]['first_name'] == response.json()[
-            'result'][1]['first_name']
-
-        assert read_mentees.json()['result'][0]['first_name'] == 'John'
-    
-    def test_search(self):
+    def test_search(self, query):
         """ Returns array of records that loosely match the search,
         automatically ordered by relevance. """
 
         response = self.client.post("/Mentees/search?search=Python")
 
         assert response.json()['result'][0]['subject'] == "Data Science: Python",\
-            'Expected Data Science: Python.  Got ' + str(response.json()['result'][0]['subject'])
+            'test_search: Expected Data Science: Python. Got ' + \
+            str(response.json()['result'][0]['subject'])
 
     def test_match(self, mentee_id: int, n_matches: int):
         """ Returns array of mentor matches for any given mentee_id. """
@@ -112,17 +85,16 @@ class TestingPlatform():
             "/Mentors/read", json={'profile_id': mentors_id})
 
         read_mentees = self.client.post(
-            "/Mentees/read", json={'profile_id': profile_id})
+            "/Mentors/read", json={'profile_id': mentee_id})
 
         '''check if their subjects indeed match'''
+
         assert read_mentors.json()['result'][0]['subject'] == read_mentees.json()[
             'result'][0]['subject'],\
-            'Expected subjects to match got ' + str(read_mentors.json()['result'][0]['subject'])\
-            + ' : ' + str(read_mentees.json()['result'][0]['subject']) 
+            'test_match: Expected subjects to match got ' + str(read_mentors.json()['result'][0]['subject'])\
+            + ' : ' + str(read_mentees.json()['result'][0]['subject'])
 
-    def test_create_delete(self, collection: str):
-        """ Creates and Deletes collection  """
-
+    def test_create(self):
         response = self.client.post("/Mentees/create", data='''{
             "profile_id": "test001",
             "first_name": "Luca",
@@ -146,24 +118,40 @@ class TestingPlatform():
             "other_info": "Notes"
             }''')
 
-        assert response.json()['result']['profile_id'] == 'test001'
+        read = self.client.post(
+            "Mentees/read", json={'profile_id': 'test001'})
+        assert read.json()['result'][0] == response.json()['result'], pprint(
+            f"test_create: Expected {str(response.json())}.\n\nGot " + str(read.json()))
+
+    def test_delete(self):
+        """ Deletes mentor/mentee in a collection"""
         response = self.client.delete("/Mentees/delete/test001")
-        read = self.client.post("/Mentees/read", json={'profile_id': 'test001'})
-        assert read.json()['result'] == [], 'Profile_id was not deteted'
 
-
-    def test_all(self):
+    def test_integration(self):
         """Tests all api endpoints"""
-        self.test_version()
         self.test_read()
-        self.test_collections()
-        self.test_create_delete()
+
+        # if read works then we can use it as the basis for the rest.
+        self.test_create()
         self.test_update()
-        self.test_collection_search()
-        self.test_match()
+
         self.test_delete()
 
+    def populate_db(self):
+        API.db.reset_collection("Mentees")
+        API.db.make_field_unique("Mentees", "profile_id")
+        API.db.create_many("Mentees", (vars(Mentee()) for _ in range(100)))
+        API.db.reset_collection("Mentors")
+        API.db.make_field_unique("Mentors", "profile_id")
+        API.db.create_many("Mentors", (vars(Mentor()) for _ in range(20)))
 
 if __name__ == '__main__':
     test_api = TestingPlatform()
+    
+    if test_api.repopulate:
+        test_api.populate_db()
+    
     test_api.test_version()
+    test_api.test_collections()
+
+    test_api.test_integration()
