a1f7b0d1866709f49304c241ba7d0d5f7aedb67b
Evgeny Khoroshukhin <horoshuhin@gmail.com>
2022-03-03 02:12:31 -0500

    removes duplicate code

--- app/vader_sentiment.py
+++ app/vader_sentiment.py
@@ -10,20 +10,8 @@ class SentimentAnalyzer:
         return self.analyzer.polarity_scores(text)["compound"]
 
 
-def sentiment_analysis() -> Callable:
-    analyzer = SentimentIntensityAnalyzer()
-
-    def worker(text) -> float:
-        return analyzer.polarity_scores(text)["compound"]
-
-    return worker
-
-
 sa_class = SentimentAnalyzer()
 print(sa_class("Some text"))
-
-sa_func = sentiment_analysis()
-print(sa_func("Some text"))
 
 
 positive = "👍"
