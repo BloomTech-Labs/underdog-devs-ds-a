408bb454b1d2ae0dc16f9b9fa4125724a0b58301
yefim-g <ygorodnitskiy@gmail.com>
2022-03-11 15:02:17 -0500

    small fix 2

--- data_generators/user_generators.py
+++ data_generators/user_generators.py
@@ -62,7 +62,7 @@ class Mentee:
             self.pair_programming = percent_true(60)
         self.your_hope = 'String'
         self.need = choice(resource_items)
-        self.parole_restriction = ''
+        self.parole_restriction = percent_true(50)
         self.disability = choice(disability)
         self.work_status = choice(work_status)
         self.assistance = choice(receiving_assistance)
