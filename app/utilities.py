24bf451ca2dc9c02733702fad38d89293f5e7c5a
NPAPENBURG <60982660+NPAPENBURG@users.noreply.github.com>
2022-03-11 21:20:34 -0500

    Updated the financial_aid function to return the probability
    Co-Authored-By: Yefim Gorodnitskiy <ygorodnitskiy@gmail.com>
    Co-Authored-By: Dagim Bantikassegn <91219777+DagimB@users.noreply.github.com>
    Co-Authored-By: Jonathan McGraw <24931398+jonathan-mcgraw@users.noreply.github.com>
    Co-Authored-By: TedKim <tedkimemail@yahoo.com>
    Co-Authored-By: Maxwell Johnson <86576382+WaylayOvercast@users.noreply.github.com>

--- app/utilities.py
+++ app/utilities.py
@@ -1,10 +1,25 @@
 from typing import Dict
+from app.data import MongoDB
 
+db = MongoDB("UnderdogDevs")
 
 def dict_to_str(data: Dict) -> str:
     """Convert dictionaries into easy to read strings."""
     return "\n" + "\n".join(f"{k}: {v}" for k, v in data.items())
 
 def financial_aid(profile_id):
-    return profile_id
+    '''This is a function that queries the database
+    for variables attached to a mentees profile id and applies
+    that information into a forumla that returns financial aid
+    probability'''
+
+    profile = db.read('Mentees', {"profile_id": profile_id})
+
+    E_l_dict = {"Beginner" : 0, "Intermediate" : 1, "Advanced" : 2, "Expert" : 3}
+
+    formerly_incarcerated = 1 if profile['result'][0]['formerly_incarcerated'] == 'true' else 0 
+    low_income = 1 if profile['result'][0]['low_income'] == 'true' else 0
+    experience_level = E_l_dict[profile['result'][0]['experience_level']]
+
+    return (((formerly_incarcerated/2 + low_income + pow(9.9*(experience_level), -1) - 0.025) / 1.577))
 
