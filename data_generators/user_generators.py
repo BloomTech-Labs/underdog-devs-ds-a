681c15bedc0ac4a1f87c016b9df1bb7f39a54ebf
yefim-g <ygorodnitskiy@gmail.com>
2022-03-21 12:20:13 -0400

    updating and resolving conflict

--- data_generators/user_generators.py
+++ data_generators/user_generators.py
@@ -8,7 +8,7 @@ from data_generators.data_options import *
 
 
 class Mentor:
-    '''Mentor schema'''
+    """Mentor Schema"""
 
     def __init__(self):
         self.profile_id = generate_uuid(16)
@@ -24,8 +24,7 @@ class Mentor:
             "Macrohard",
             "Pineapple",
         ])
-        self.tech_stack = choice(subjects)
-        self.experience_level = choice(skill_levels)
+        self.tech_stack = choice(skill_levels)
         self.job_help = self.tech_stack == "Career Development"
         self.industry_knowledge = percent_true(90)
         self.pair_programming = percent_true(90)
@@ -35,7 +34,7 @@ class Mentor:
 
 
 class Mentee:
-    '''Mentee schema'''
+    """Mentee Schema"""
 
     def __init__(self):
         self.profile_id = generate_uuid(16)
@@ -60,7 +59,7 @@ class Mentee:
             self.pair_programming = False
         else:
             self.pair_programming = percent_true(60)
-        self.your_hope = 'String'
+        self.your_hope = "String"
         self.need = choice(resource_items)
         self.parole_restriction = percent_true(50)
         self.disability = choice(disability)
@@ -94,23 +93,20 @@ class MenteeFeedback:
 
 
 class Meeting:
-    '''Create dummy meeting record which is stored in
-    meeting collection.'''
+    """Create dummy meeting record which is stored in
+    meeting collection."""
 
     def __init__(self, mentee_id, mentor_id):
-        self.meeting_id = randint(1, 10000)
-        self.host_id = mentor_id
-        self.attendee_id = mentee_id
-        '''The following date data may need conversion
-        depending how the backend sends their data to us
-        They are currently set as a static datetime object'''
+        self.meeting_id = generate_uuid(16)
         self.created_at = "2018-06-12T09:55:22"
         self.updated_at = "2018-06-12T09:55:22"
-        self.meeting_date = "2018-06-12T09:55:22"
-        self.meeting_time = "2018-06-12T09:55:22"
         self.meeting_topic = choice(topics)
-        self.attended = choice([0, 1])
+        self.meeting_start_date = "2018-06-12T09:55:22"
+        self.meeting_end_date = "2018-06-12T09:55:22"
+        self.host_id = mentor_id
+        self.attendee_id = mentee_id
         self.meeting_notes = "Meeting notes here!"
+        self.meeting_missed = choice(['Missed', 'Attended'])
 
     def __str__(self):
         return "\n".join(f"{k}: {v}" for k, v in vars(self).items())
@@ -119,13 +115,13 @@ class Meeting:
 if __name__ == "__main__":
     db = MongoDB("UnderdogDevs")
 
-    # db.reset_collection("Mentees")
-    # db.get_collection("Mentees").create_index("profile_id", unique=True)
-    # db.create_many("Mentees", (vars(Mentee()) for _ in range(100)))
+    db.reset_collection("Mentees")
+    db.get_collection("Mentees").create_index("profile_id", unique=True)
+    db.create_many("Mentees", (vars(Mentee()) for _ in range(100)))
 
-    # db.reset_collection("Mentors")
-    # db.get_collection("Mentors").create_index("profile_id", unique=True)
-    # db.create_many("Mentors", (vars(Mentor()) for _ in range(20)))
+    db.reset_collection("Mentors")
+    db.get_collection("Mentors").create_index("profile_id", unique=True)
+    db.create_many("Mentors", (vars(Mentor()) for _ in range(20)))
 
     mentees = db.read("Mentees")
     mentors = db.read("Mentors")
