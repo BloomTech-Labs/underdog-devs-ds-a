95ec8433ed20e5821021ef71b41708a028f1e87d
ad_victoriam_101 <keeper24@gmail.com>
2022-04-04 12:30:27 -0700

    added NLTK folder and pathing to app to allow functionality. changed lines 13-15 on requirements.txt changed 8-19 & 47-85 in utilities.py

--- app/utilities.py
+++ app/utilities.py
@@ -1,15 +1,21 @@
 from typing import Dict
 
+# General Imports
+from dataclasses import dataclass
 
 # import for nltk
 import nltk
+
+nltk.data.path.append("./nltk_files/")
 from nltk.tokenize import word_tokenize
 from nltk.corpus import stopwords
 from nltk.stem import WordNetLemmatizer
 
 # imports from Gemsin
+
 from gensim.models.ldamodel import LdaModel
 from gensim.corpora.dictionary import Dictionary
+
 
 def dict_to_str(data: Dict) -> str:
     """Convert dictionaries into easy to read strings."""
@@ -19,7 +25,7 @@ def dict_to_str(data: Dict) -> str:
 def financial_aid_gen(profile):
     """This is a function that queries the database
     for variables attached to a mentees profile id and applies
-    that information into a forumla that returns financial aid
+    that information into a formula that returns financial aid
     probability"""
 
     e_l_dict = {"Beginner": 1, "Intermediate": 2, "Advanced": 3, "Expert": 4}
@@ -34,8 +40,46 @@ def financial_aid_gen(profile):
 
         f_i /= 2
         l_i += f_i
-        e_l = l_i + pow(9.9*e_l, -1)
+        e_l = l_i + pow(9.9 * e_l, -1)
         return (e_l - 0.025) / 1.577
 
     return f"{f_a_func(f_i, l_i, e_l):.2%}"
-#nltk.data.path.append("./nltk_files")
+
+
+def topic_function(notes, num_topic=1):
+    """
+    Takes a given set of notes and returns topic analysis of the given text document.
+    notes: str, text document or string to get topic analysis on.
+    num_topic: int, amount of topics to be returned from.
+    """
+
+    temp = []
+
+    # custom stop word dictionary.
+    stops = ['ha', 'got']
+
+    # Lamentize and remove stop words
+
+    for char in word_tokenize(notes):
+        if char.isalpha():
+            char = WordNetLemmatizer().lemmatize(char.lower())
+            if char not in set(stopwords.words('english') + stops):
+                temp.append(char)
+
+    temp2 = [temp]
+
+    # Create corpus
+    corpus_dict = Dictionary(temp2)
+    corpus = [corpus_dict.doc2bow(x) for x in temp2]
+
+    # Create model
+    lda = LdaModel(corpus, num_topics=num_topic,
+                   random_state=69,
+                   id2word=corpus_dict)
+    topic = list(lda.id2word.values())
+
+    if num_topic == 1:
+        return topic[0]
+    else:
+        return topic[0:num_topic]
+
