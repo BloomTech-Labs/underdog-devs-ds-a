7d8057b080907c7d220d0d92d40d6383690a424b
Brogan Stich <keeper24@gmail.com>
2022-04-06 14:34:29 -0700

    New branch from main, removes old files from seperate branch. same branch as Feature/numeric_sentiment_analysis

--- app/vader_sentiment.py
+++ app/vader_sentiment.py
@@ -4,9 +4,11 @@ from vaderSentiment.vaderSentiment import SentimentIntensityAnalyzer
 vader = SentimentIntensityAnalyzer()
 
 
-def vader_score(text: str) -> str:
+def vader_score(text: str, numerical: bool):
     """Return compound scores of text in list using vader analysis."""
     score = vader.polarity_scores(text)["compound"]
+    if numerical:
+        return score
     if score > 0.3:
         return "Positive"
     elif score < -0.3:
@@ -16,4 +18,13 @@ def vader_score(text: str) -> str:
 
 
 if __name__ == '__main__':
-    print(vader_score("good but not great"))
+    print(vader_score("good but not great", False))
+    """
+    This is to test the expected result requested in api.py
+
+    """
+    # Numeric
+    print({"Result": vader_score("Astring is so cool wow hot damn", True)})
+    # String text.
+    print({"Result": vader_score("Astring is so cool wow hot damn", False)})
+
