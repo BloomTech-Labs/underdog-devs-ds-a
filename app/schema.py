7178bf48af82194dcdef4cffb140993807646cf6
GermanParra <gerpar13@gmail.com>
2022-04-21 20:49:39 -0400

    Update schema.py

--- app/schema.py
+++ app/schema.py
@@ -1,7 +1,11 @@
+import string
+from itertools import chain
+from math import ceil, floor
+from random import choices, shuffle
 from typing import List, Literal, Optional
 from datetime import datetime
 from pydantic import BaseModel, constr, Field
-from uuid import uuid4, UUID
+
 
 class Variants:
     exp_levels = Literal[
@@ -14,20 +18,31 @@ class Variants:
         "General Programming",
     ]
 
+    def generate_uuid(n_len: int) -> str:
+        n1 = ceil(n_len / 2)
+        n2 = floor(n_len / 2)
+        prefix = choices(string.ascii_letters, k=n1)
+        suffix = map(str, choices(range(0, 9), k=n2))
+        uuid_list = list(chain(prefix, suffix))
+        shuffle(uuid_list)
+        uuid = "".join(uuid_list)
+        return uuid
+
+
 class Mentor(BaseModel):
-    profile_id: constr(max_length=16)         
-    first_name: constr(max_length=255) = "Maria"
-    last_name: constr(max_length=255) = "Perez"      
-    tech_stack: constr(max_length=255) = "App Dev"
-    experience_level: Variants.exp_levels = "Beginner"
+    profile_id: constr(max_length=16) = Variants.generate_uuid(16)    
+    first_name: constr(max_length=255)
+    last_name: constr(max_length=255)    
+    tech_stack: constr(max_length=255) 
+    experience_level: Variants.exp_levels
     job_help: Optional[bool] = False
-    industry_knowledge: bool    = False
-    pair_programming: bool   = False
+    industry_knowledge: Optional[bool] = True
+    pair_programming: Optional[bool] = True
 
 class Mentee(BaseModel):
-    profile_id: constr(max_length=16) #UUID = Field(default_factory=uuid4)
+    profile_id: constr(max_length=16) = Variants.generate_uuid(16)
     first_name: constr(max_length=255) 
-    last_name: constr(max_length=255) = None
+    last_name: constr(max_length=255)
     formerly_incarcerated: Optional[bool] = False
     underrepresented_group: Optional[bool] = False
     low_income: Optional[bool] = False
@@ -35,34 +50,35 @@ class Mentee(BaseModel):
     tech_stack: constr(max_length=255)
     experience_level: Variants.exp_levels  
     job_help: Optional[bool] = False
-    pair_programming: bool
+    pair_programming: Optional[bool] = True
     need: constr(max_length=255) = None
-    parole_restriction: bool
-    disability: bool
-    work_status: bool
-    assistance: bool
+    parole_restriction: Optional[bool] = False
+    disability: Optional[bool] = False
+    work_status: Optional[bool] = True
+    assistance: Optional[bool] = False
 
 class Meeting(BaseModel):
-    meeting_id: str
-    created_at: datetime
-    updated_at: datetime
-    meeting_topic: str
-    meeting_start_date: datetime
-    meeting_end_date: datetime
-    host_id: str
-    attendee_id: str
-    meeting_notes = constr(max_length=2000)
-    meeting_missed = Literal['Missed', 'Attended']
+    meeting_id: constr(max_length=16) = Variants.generate_uuid(16)
+    created_at: datetime = str(datetime.now())
+    updated_at: datetime = str(datetime.now())
+    meeting_topic: constr(max_length=255) 
+    meeting_start_date: datetime   
+    meeting_end_date: datetime   
+    host_id: constr(max_length=16)  
+    attendee_id: constr(max_length=16)
+    meeting_notes: constr(max_length=2000) 
+    meeting_missed: Literal['Missed', 'Attended'] 
 
 class Feedback(BaseModel):
-    ticket_id: str
-    mentee_id: str
-    mentor_id: str
+    ticket_id: constr(max_length=16) = Variants.generate_uuid(16)
+    mentee_id: constr(max_length=16)
+    mentor_id: constr(max_length=16)
     feedback: constr(max_length=2000)
 
 class Resource(BaseModel):
     name: constr(max_length=255)
-    item_id: str
+    item_id: constr(max_length=16) = Variants.generate_uuid(16)
+
 
 if __name__ == '__main__':
     M = Mentor()
