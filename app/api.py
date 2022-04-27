cd174450d1fca9f17a2df432916b1f48df855481
Adam Vu <adamvu1087@gmail.com>
2022-04-27 16:03:34 -0400

    Updated API

--- app/api.py
+++ app/api.py
@@ -63,7 +63,6 @@ async def computer_assignment_rating_visualizer():
 async def create_mentor(data: Mentor):
     """Create a new record in the Mentors collection,
     validating input fields using Pydantic schema.
-
     Args:
         data (Mentor): Mentor class (Pydantic BaseModel) object
     Returns:
@@ -75,11 +74,9 @@ async def create_mentor(data: Mentor):
 @API.post("/read/mentor")
 async def read(data: Optional[Dict] = None):
     """Return array of records that exactly match the given query from Mentors.
-
     Queries from Mentors collection with optional filters
     given (data). If no filtering data is given, will return all
     documents within Mentors collection.
-
     Args:
         data (dict) (optional): Key value pairs to match
     Returns: List of all matching documents in the Mentors collection
@@ -90,11 +87,9 @@ async def read(data: Optional[Dict] = None):
 @API.post("/update/mentor")
 async def update(query: Dict, update_data: Dict):
     """Updates Mentor documents that statisfy the query with update_data.
-
     Queries from Mentor Collection with filters given (query).
     Validate changes in update_data using MentorUpdate class (Pydantic schema)
     and updates the corresponding fields, by overwriting or adding data.
-
     Args:
         query (dict): Key value pairs to filter for
         update_data (dict): Key value pairs to update
@@ -107,20 +102,53 @@ async def update(query: Dict, update_data: Dict):
     return {"result": API.db.read("Mentors", query)}
 
 
+@API.post("/create/mentee")
+async def create_mentee(data: Mentee):
+    """Create a new record in the Mentees collection. Creates new document
+    within Mentees using the data parameter to populate its fields.
+    Args:
+        data (dict): Key value pairs to be mapped to document fields
+    Returns: New record data as dictionary
+    """
+    return {"result": API.db.create("Mentees", data.dict())}
+
+
+@API.post("/read/mentee")
+async def read(data: Optional[Dict] = None):
+    """Return array of records that exactly match the given query from Mentees.
+    Queries from Mentees collection with optional filters given (data).
+    If no filtering data is given, will return all documents within collection.
+    Args:
+        data (dict) (optional): Key value pairs to match
+    Returns: List of all matching documents
+    """
+    return {"result": API.db.read("Mentees", data)}
+
+
+@API.post("/update/mentee")
+async def update(query: Dict, update_data: Dict):
+    """Updates Mentee documents that statisfy the query with update_data.
+    Queries from Mentee Collection with filters given (query).
+    Then updates fields using update_data, by overwriting or adding data.
+    Args:
+        query (dict): Key value pairs to filter for
+        update_data (dict): Key value pairs to update
+    Returns: None
+    """
+    MenteeUpdate(**update_data)
+    return {"result": API.db.update("Mentees", query, update_data)}
+
+
 @API.post("/{collection}/create")
 async def create(collection: str, data: Dict):
     """Create a new record in the given collection.
-
     Creates new document within given collection using the data
     parameter to populate its fields.
-
     Args:
         collection (str): Name of collection retrieved from URL
         data (dict): Key value pairs to be mapped to document fields
-
         Input Example:
         collection = "Mentees"
-
     Returns:
         New collection's data as dictionary
     """
@@ -131,15 +159,12 @@ async def create(collection: str, data: Dict):
 @API.post("/{collection}/read")
 async def read(collection: str, data: Optional[Dict] = None):
     """Return array of records that exactly match the given query.
-
     Defines collection from URL and queries it with optional filters
     given (data). If no filtering data is given, will return all
     documents within collection.
-
     Args:
         collection (str): Name of collection retrieved from URL
         data (dict) (optional): Key value pairs to match
-
     Returns:
         List of all matching documents
     """
@@ -150,16 +175,13 @@ async def read(collection: str, data: Optional[Dict] = None):
 @API.post("/{collection}/update")
 async def update(collection: str, query: Dict, update_data: Dict):
     """Update collection and return the number of updated documents.
-
     Defines collection from URL and queries it with filters
     given (query). Then updates fields using update_data, either adding
     or overwriting data.
-
     Args:
         collection (str): Name of collection retrieved from URL
         query (dict): Key value pairs to filter for
         update_data (dict): Key value pairs to update
-
     Returns:
         Integer count of updated documents
     """
@@ -170,15 +192,12 @@ async def update(collection: str, query: Dict, update_data: Dict):
 @API.post("/{collection}/search")
 async def collection_search(collection: str, search: str):
     """Return list of docs loosely matching string, sorted by relevance.
-
     Searches collection given in URL for documents that approximate the
     given string (search), and then presents them, automatically
     ordering results by relevance to the search.
-
     Args:
         collection (str): Name of collection to query
         search (str): Querying parameter
-
     Returns:
         List of queried documents
     """
@@ -189,15 +208,12 @@ async def collection_search(collection: str, search: str):
 @API.post("/match/{profile_id}")
 async def match(profile_id: str, n_matches: int):
     """Return an array of mentor matches for any given mentee profile_id.
-
     Utilizes imported MatcherSortSearch() to query database for the
     given number of mentors that may be a good match for the given
     mentee. See documentation for MatcherSortSearch() for details.
-
     Args:
         profile_id (str): ID number for mentee needing a mentor
         n_matches (int): Maximum desired matching candidates
-
     Returns:
         List of mentor IDs
     """
@@ -207,17 +223,14 @@ async def match(profile_id: str, n_matches: int):
 @API.post("/match_resource/{item_id}")
 async def match_resource(item_id: str, n_matches: int):
     """ Returns array of mentee matches for any given Resource item_id.
-
     Utilizes imported MatcherSortSearchResource() to query database for the
     given number of mentees that may be a good match for the given
     Resource(s). See documentation for MatcherSortSearchResource() for details.
-
     Args:
         item_id (int):
             ID number for resource item to be allocated to a mentee
         n_matches (int):
             Maximum desired matching candidates. Ideally should be 1.
-
     Returns:
         List of mentee ID(s) """
     return {"result": API.resource_matcher(n_matches, item_id)}
@@ -226,14 +239,11 @@ async def match_resource(item_id: str, n_matches: int):
 @API.delete("/{collection}/delete/{profile_id}")
 async def delete(collection: str, profile_id: str):
     """Removes a user from the given collection.
-
     Deletes all documents containing the given profile_id permanently,
     and returns the deleted profile_id for confirmation.
-
     Args:
         collection (str): Name of collection to query for deletion
         profile_id (str): ID number of user to be deleted
-
     Returns:
         Dictionary with key of "deleted" and value of the profile_id
     """
@@ -261,14 +271,11 @@ async def all_exception_handler(request: Request, exc: Exception):
 @API.post("/financial_aid/{profile_id}")
 async def financial_aid(profile_id: str):
     """Returns the probability that financial aid will be required.
-
     Calls the financial aid function from functions.py inputing the
     profile_id for calculation involving formally incarcerated, low income,
     and experience level as variables to formulate probability of financial aid
-
     Args:
         profile_id (str): the profile id of the mentee
-
     Returns:
         the probability that financial aid will be required
     """
@@ -286,7 +293,6 @@ async def sentiment(text: str):
     """ Returns positive, negative or neutral sentiment of the supplied text.
     Args:
         text (str): the text to be analyzed
-
     Returns:
         positive/negative/neutral prediction based on sentiment analysis
     """
