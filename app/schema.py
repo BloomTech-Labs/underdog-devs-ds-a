dc85a154ad2134acd5335e4f86841c3485b2621b
Adam Vu <adamvu1087@gmail.com>
2022-05-02 11:28:31 -0400

    Reworked Variable and Removed Exp

--- app/schema.py
+++ app/schema.py
@@ -2,7 +2,6 @@ from typing import List, Literal, Optional
 from datetime import datetime
 from pydantic import BaseModel, constr
 
-
 class Mentor(BaseModel):
     profile_id: constr(max_length=255)
     first_name: constr(max_length=255)
@@ -14,6 +13,7 @@ class Mentor(BaseModel):
     city: constr(max_length=255)
     current_company: constr(max_length=255)
     current_position: constr(max_length=255)
+    tech_stack: constr(max_length=255)
     able_to_commit: bool
     mentor_contribution: List[constr(max_length=255)]
     how_heard_about_us: constr(max_length=255)
@@ -31,6 +31,7 @@ class MentorUpdate(BaseModel):
     city: Optional[constr(max_length=255)]
     current_company: Optional[constr(max_length=255)]
     current_position: Optional[constr(max_length=255)]
+    tech_stack: constr(max_length=255)
     able_to_commit: Optional[bool]
     mentor_contribution: Optional[List[constr(max_length=255)]]
     how_heard_about_us: Optional[constr(max_length=255)]
@@ -50,6 +51,7 @@ class Mentee(BaseModel):
     underrepresented_group: bool
     low_income: bool
     list_convictions: List[constr(max_length=255)]
+    tech_stack: constr(max_length=255)
     looking_for: List[constr(max_length=255)]
     anything_else: Optional[constr(max_length=2500)]
 
@@ -67,6 +69,7 @@ class MenteeUpdate(BaseModel):
     underrepresented_group: Optional[bool]
     low_income: Optional[bool]
     list_convictions: Optional[List[constr(max_length=255)]]
+    tech_stack: constr(max_length=255)
     looking_for: Optional[List[constr(max_length=255)]]
     anything_else: Optional[constr(max_length=2500)]
 
