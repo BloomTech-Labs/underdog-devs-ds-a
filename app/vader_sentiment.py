3bb196122f153e579e6cec456e8dbe2c2dc0023c
Brogan Stich <keeper24@gmail.com>
2022-05-04 10:25:31 -0700

    PEP8 Cleanup check for .py files

--- app/vader_sentiment.py
+++ app/vader_sentiment.py
@@ -5,7 +5,11 @@ vader = SentimentIntensityAnalyzer()
 
 
 def vader_score(text: str):
-    """Return an intuitive string (Positive, Negative or Neutral) base on the compound score of text from vader analysis."""
+    """
+    Return an intuitive string
+    (Positive, Negative or Neutral)
+    base on the compound score of text from vader analysis.
+    """
     score = vader.polarity_scores(text)["compound"]
     if score > 0.3:
         return "Positive"
@@ -16,13 +20,9 @@ def vader_score(text: str):
 
 
 if __name__ == '__main__':
-    print(vader_score("good but not great"))
     """
     This is to test the expected result requested in api.py
-
     """
-    # Numeric
-    print({"Result": vader_score("Astring is so cool wow hot damn")})
-    # String text.
-    print({"Result": vader_score("Astring is so cool wow hot damn")})
+    # String text
+    print({"Result": vader_score("A string is so cool wow, I hope this is a positive.")})
 
