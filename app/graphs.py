3e312e50315c38cd23b80e56cc0a750285390202
Anna Koduru <annakoduru@gmail.com>
2022-06-21 12:17:26 -0400

    Update graphs.py
    Added visualization to display mentors' vader sentiment scores in an interactive bar chart showing feedback from mentees' over time.

--- app/graphs.py
+++ app/graphs.py
@@ -12,3 +12,31 @@ def tech_stack_by_role(dataframe: pd.DataFrame):
         color=alt.Color('user_role', title="User Role"),
         tooltip=alt.Tooltip(list(dataframe.columns)),
     )
+
+def feedback(dataframe):
+  interval = alt.selection_interval(encodings=['x'])
+
+  source = dataframe
+
+  bar = alt.Chart(source).mark_bar().encode(
+      x='datetime:T',
+      y='vader_score:Q',
+      color=alt.condition(interval, 'mentor_id:N',
+                          alt.value('lightgray'))
+  ).properties(
+      width=900,
+      selection=interval
+  )
+  line = alt.Chart(source).mark_line(color='red').transform_window(
+      # The field to average
+      rolling_mean='mean(vader_score)',
+      # The number of values before and after the current value to include.
+      frame=[-9, 0]
+  ).encode(
+      x='datetime',
+      y='rolling_mean:Q'
+  ).properties(width=900
+  ).transform_filter(
+      interval
+  )
+  return bar & line
