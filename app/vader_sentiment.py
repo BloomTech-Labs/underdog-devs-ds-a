f838bc059cb5fcb3431fb983725eec724a877617
Stephen Spradling <94577074+sspradling78@users.noreply.github.com>
2022-07-11 11:08:15 -0700

    Update vader_sentiment.py

--- app/vader_sentiment.py
+++ app/vader_sentiment.py
@@ -3,12 +3,17 @@ analyzer = SentimentIntensityAnalyzer()
 
 def vader_score(text: str):
     """
-    Returns vader scores for feedback
+    Return an intuitive string
+    (Positive, Negative or Neutral)
+    base on the compound score of text from vader analysis.
     """
-    score = analyzer.polarity_scores(text)['compound']
-    if score > .3:
-        return ['positive', score]
-    return score
+    score = vader.polarity_scores(text)["compound"]
+    if score > 0.3:
+        return "Positive"
+    elif score < -0.3:
+        return "Negative"
+    else:
+        return "Neutral"
 
 
 if __name__ == '__main__':
