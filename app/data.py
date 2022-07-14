a4290144e49d2a1377eb075aa15f4daf28625f88
Stephen Spradling <94577074+sspradling78@users.noreply.github.com>
2022-07-14 08:44:42 -0700

    Updated docstring to projection() in data.py

--- app/data.py
+++ app/data.py
@@ -131,19 +131,18 @@ class MongoDB:
     def projection(self, collection: str, query: Dict, projection: Dict) -> List[Dict]:
         """Query collection with specific parameters.
 
-                Searches the given collection with the default parameter of
-                _id: false and the option to include further parameters. Search
-                parameters must be formatted as a dictionary.
+        Searches the given collection with the default parameter of
+        _id: false and the option to include further parameters. Search
+        parameters must be formatted as a dictionary.
 
-                Args:
-                    collection (str): name of collection to query
-                    query (dict): List of key value pairs to match
-                    projection (dict): _id: False, search for other
-                    parameters to match
+        Args:
+            collection (str): name of collection to query
+            query (Dict): List of key value pairs to match
+            projection (Dict): _id: False, search for other
 
-                Returns:
-                    List of all documents matching query parameters
-                """
+        Returns:
+            List of all documents matching query parameters
+        """
         projection["_id"] = False
         return list(self.get_collection(collection).find(query, projection))
 
