34b297dc0cc23fed445cebcff3be52e29c91c07f
jacksemder <jacksemder@gmail.com>
2022-08-02 08:43:55 -0700

    using vars() method, from office hours

--- test_coverage/schema_validation_testing.py
+++ test_coverage/schema_validation_testing.py
@@ -1,79 +1,10 @@
-import inspect
-from app.schema import *
-#from data_generators.generators import *
+from app.schema import Mentor
+from data_generators.generators import RandomMentor
+# doctest
 
+mentor = RandomMentor()
+mentor_dict = vars(mentor)
+print(mentor_dict)
+created = mentor_dict.pop("created_at")
+Mentor(**mentor_dict)
 
-ment = Mentor
-print(inspect.getmembers(ment))
-
-
-
-
-#def validate(collection, random_collection):
-    #length1 = 0
-    #length2 = 0
-
-    #col = collection
-    #rand_col = random_collection
-
-    #for i in range(len(inspect.getmembers(col)[2][1]:
-        #length1 += 1
-    #for i in range(len(inspect.getmembers(rand_col)[2][1]:
-        #length2 += 1
-
-    #assert length1 == length2, "Collections don't match"
-    
-'''
-def validate_mentor():
-    # compare shapes
-    length1 = 0
-    length2 = 0
-    mentor = Mentor()
-    randomMentor = RandomMentor()
-    for i in range(len(inspect.getmembers(mentor)[2][1]:
-        length1 += 1
-    for i in range(len(inspect.getmembers(randomMentor)[2][1]:
-        length2 += 1
-
-    assert length1 == length2, "collections don't match"
-
-
-def validate_mentee():
-    # compare shapes
-    length1 = 0
-    length2 = 0
-    mentee = Mentee()
-    randomMentee = RandomMentee()
-    for i in range(len(inspect.getmembers(mentee)[2][1]:
-        length1 += 1
-    for i in range(len(inspect.getmembers(randomMentee)[2][1]:
-        length2 += 1
-
-    assert length1 == length2, "collections don't match"
-
-def validate_meeting():
-    # compare shapes
-    length1 = 0
-    length2 = 0
-    meeting = Meeting()
-    randomMeeting = RandomMeeting()
-    for i in range(len(inspect.getmembers(meeting)[2][1]:
-        length1 += 1
-    for i in range(len(inspect.getmembers(randomMeeting)[2][1]:
-        length2 += 1
-
-    assert length1 == length2, "collections don't match"
-
-def validate_feedback():
-    # compare shapes
-    length1 = 0
-    length2 = 0
-    feedback = Feedback()
-    randomFeedback = RandomMenteeFeedback()
-    for i in range(len(inspect.getmembers(feedback)[2][1]:
-        length1 += 1
-    for i in range(len(inspect.getmembers(randomFeedback)[2][1]:
-        length2 += 1
-
-    assert length1 == length2, "collections don't match"
-'''
