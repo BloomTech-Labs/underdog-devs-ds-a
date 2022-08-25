eaec344e5eccf10aa469c6e39488f3b4330f8a65
Jonathan Torres <j.torresespinoza02@gmail.com>
2022-08-25 11:18:31 -0700

    Updated API docstrings

--- app/api.py
+++ app/api.py
@@ -31,6 +31,15 @@ API.add_middleware(
 
 @API.get("/version")
 async def version():
+    """
+    API version and password (correct password returned in local setup).
+
+    Returns
+    ----------
+    Result: JSON Object
+        Key-value pairs for version and password.
+    """
+
     local = os.getenv("CONTEXT") == "local"
     remote = "Run the API locally with the proper environment variables"
     password = API.db.first("Secret")["Password"] if local else remote
@@ -39,21 +48,74 @@ async def version():
 
 @API.get("/collections")
 async def collections():
+    """
+    Names of collections and the count of documents in each collection.
+
+    Returns
+    ----------
+    Result: JSON Object
+        "CollectionName"(str): "count"(int)
+    """
+
     return {"result": API.db.get_database_info()}
 
 
 @API.post("/read/mentor")
 async def read_mentor(data: Optional[Dict] = None):
+    """
+    Given key-value pair Returns an array/object of mentors whose profiles meet the specified criteria,
+    or if given no input Returns data for all mentors.
+
+    Parameters
+    ----------
+    JSON Object
+        {"KeyName":Value, "KeyName2":Value2}
+            Object can contain one or more key-value pairs. For a complete list
+            of possible key-value pairs, see Example Schema in /create/mento.
+    Returns
+    ----------
+    JSON Object
+        Array of mentor profile documents.
+
+    """
+
     return {"result": API.db.read("Mentors", data)}
 
 
 @API.post("/read/mentee")
 async def read_mentee(data: Optional[Dict] = None):
+    """
+    Returns an array of all mentees whose profiles meet the specified criteria.
+
+    Parameters
+    ----------
+    JSON Object
+        {"KeyName":Value, "KeyName2":Value2}
+            Object can contain one or more key-value pairs. For a complete list
+            of possible key-value pairs, see Example Schema in /create/mentee.
+    Returns
+    ----------
+    JSON Object
+        Array of mentee profile documents.
+    """
     return {"result": API.db.read("Mentees", data)}
 
 
 @API.post("/create/mentor")
 async def create_mentor(data: Mentor):
+    """
+    Creates a mentor document (profile).
+
+    Parameters
+    ----------
+    user: JSON Object
+        See Example Value for structure and data types of key-value pairs. "referred_by" and "other_info" are optional;
+        all other fields are required.
+    Returns
+    ----------
+    success: bool
+        Indicates success or failure of document creation.
+    """
     try:
         return {"result": API.db.create("Mentors", data.dict())}
     except DuplicateKeyError:
@@ -62,36 +124,126 @@ async def create_mentor(data: Mentor):
 
 @API.post("/create/mentee")
 async def create_mentee(data: Mentee):
+    """
+    Creates a mentee document (profile).
+
+    Parameters
+    ----------
+    user: JSON Object
+        See Example Value for structure and data types of key-value pairs. "convictions" and "other_info" are optional;
+        all other fields are required.
+    Returns
+    ----------
+    success: bool
+        Indicates success or failure of document creation.
+    """
+
     try:
-        return {"result": API.db.create("Mentees", data.dict())} 
+        return {"result": API.db.create("Mentees", data.dict())}
     except DuplicateKeyError:
         raise HTTPException(status_code=409, detail="Profile ID must be unique.")
 
 
 @API.post("/update/mentor/{profile_id}")
 async def update_mentors(profile_id: str, update_data: MentorUpdate):
+    """
+    Updates a mentor document (profile).
+    Parameters
+    ----------
+    profile_id: string
+    update_data: JSON Object
+        Key-value pairs to be updated. Can accept one or more key-value pairs. See Example Value for valid key-value
+        pairs.
+    Returns
+    ----------
+    profile_id, user: array
+        Returns profile_id and updated mentor document.
+    """
+
     data = update_data.dict(exclude_none=True)
     return {"result": API.db.update("Mentors", {"profile_id": profile_id}, data)}
 
 
 @API.post("/update/mentee/{profile_id}")
 async def update_mentees(profile_id: str, update_data: MenteeUpdate):
+    """
+    Updates a mentee document (profile).
+
+    Parameters
+    ----------
+    profile_id: string
+    update_data: JSON Object
+        Key-value pairs to be updated. Can accept one or more key-value pairs. See Example Value for valid key-value
+        pairs.
+
+    Returns
+    ----------
+    profile_id, user: array
+        Returns profile_id and updated mentee document.
+    """
+
     data = update_data.dict(exclude_none=True)
     return {"result": API.db.update("Mentees", {"profile_id": profile_id}, data)}
 
 
 @API.post("/match/{profile_id}")
 async def match(profile_id: str, n_matches: int):
+    """
+    Returns n_matches mentor profiles for mentee profile_id.
+
+    Parameters
+    ----------
+    profile_id: str
+    n_matches: int
+
+    Returns
+    ----------
+    profile_id : array
+
+    """
+
     return {"result": API.matcher(n_matches, profile_id)}
 
 
 @API.post("/read/feedback")
 async def read_feedback(query: FeedbackOptions):
+    """
+    Returns mentee feedback.
+
+    Parameters
+    ----------
+    ticket_id: str
+    mentee_id: str
+    mentor_id: str
+
+    Returns
+    ---------
+    Feedback JSON Object
+
+    """
+
     return {"result": API.db.read("Feedback", query.dict(exclude_none=True))}
 
 
 @API.post("/create/feedback")
 async def create_feedback(data: Feedback):
+    """
+    Returns vadersentiment analysis for mentor and mentee feedback in the form of an array/object.
+
+    Parameters
+    ----------
+    text: str
+    ticket_id: str
+    mentee_id: str
+    mentor_id: str
+
+    Return
+    ---------
+    Feedback JSON Object.
+        Returns parameters and vader_score.
+
+    """
+
     data_dict = data.dict(exclude_none=True)
     data_dict["vader_score"] = sentiment_rank(data_dict["text"])
     return {"result": API.db.create("Feedback", data_dict)}
@@ -99,12 +251,40 @@ async def create_feedback(data: Feedback):
 
 @API.delete("/delete/feedback")
 async def delete_feedback(ticket_id: str):
+    """
+    Deletes mentee feedback.
+
+    Parameters
+    ----------
+    ticket_id: str
+
+    Returns
+    ----------
+    JSON object with deletion confirmation.
+    """
+
     API.db.delete("Feedback", {"ticket_id": ticket_id})
     return {"result": {"deleted": ticket_id}}
 
 
 @API.patch("/update/feedback")
 async def update_feedback(ticket_id: str, update_data: FeedbackUpdate):
+    """
+    Updates mentee feedback.
+
+    Parameters
+    ----------
+    ticket_id: str
+    text: str
+    mentee_id: str
+    mentor_id: str
+
+    Returns
+    ----------
+    JSON object with updated vader_score.
+
+    """
+
     data_dict = update_data.dict(exclude_none=True)
     data_dict["vader_score"] = sentiment_rank(data_dict["text"])
     return {"result": API.db.update("Feedback", {"ticket_id": ticket_id}, data_dict)}
@@ -112,8 +292,15 @@ async def update_feedback(ticket_id: str, update_data: FeedbackUpdate):
 
 @API.get("/graph/tech-stack-by-role")
 async def tech_stack_by_role():
-    """ Tech Stack Count by Role - stacked bar chart
-    Returns an Altair Chart in JSON format """
+    """
+    Tech Stack Count by Role - stacked bar chart.
+
+
+    Returns
+    ----------
+    Altair Chart in JSON format.
+
+    """
     return stacked_bar_chart(
         df_tech_stack_by_role(API.db),
         "tech_stack",
