dfad11f23c65ec01353c9e64c53341ce7e198911
Robert Sharp <webmaster@sharpdesigndigital.com>
2022-09-13 06:52:43 -0700

    Update model_router.py

--- app/routers/model_router.py
+++ app/routers/model_router.py
@@ -12,4 +12,9 @@ Router.matcher = MatcherSortSearch()
 
 @Router.post("/match/{profile_id}")
 async def match(profile_id: str, n_matches: int):
+    """Returns n_matches mentor profiles for mentee profile_id
+    <pre><code>
+    @param profile_id: str
+    @param n_matches: Optional[int] = None
+    @return JSON[Array[profile_id]]</pre></code>"""
     return {"result": Router.matcher(n_matches, profile_id)}
