d9d3acbd843b3a9c5b057de1d3b2e34b0b4c3a78
Robert Sharp <webmaster@sharpdesigndigital.com>
2022-09-13 06:50:09 -0700

    Update graph_router.py

--- app/routers/graph_router.py
+++ app/routers/graph_router.py
@@ -11,8 +11,9 @@ Router.db = MongoDB()
 
 @Router.get("/graph/tech-stack-by-role")
 async def tech_stack_by_role():
-    """ Tech Stack Count by Role - stacked bar chart
-    Returns an Altair Chart in JSON format """
+    """Tech Stack Count by Role, stacked bar chart
+    <pre><code>
+    @return JSON[Altair.Chart]</pre></code>"""
     return stacked_bar_chart(
         df_tech_stack_by_role(Router.db),
         "tech_stack",
