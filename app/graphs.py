585bdaf7dd2b0e0b76225ff73cb3bb51ca0f0d68
zoebrantley <101472453+zoebrantley@users.noreply.github.com>
2022-10-05 16:19:14 -0400

    updates to graphs.py, added BL-969 visualization functions.

--- app/graphs.py
+++ app/graphs.py
@@ -43,3 +43,63 @@ def df_tech_stack_by_role(database: MongoDB) -> DataFrame:
     mentors = mentors.explode(column="tech_stack")
     mentors["role"] = "Mentor"
     return pandas.concat([mentees, mentors])
+
+
+def meeting_chart(df, column_1, column_2):
+    return Chart(
+        df,
+        title=f"{title_fix(column_1)}",
+    ).mark_bar().encode(
+        x=X(column_1, title=title_fix(column_1), sort="-y"),
+        y=Y(f"count({column_1})"),
+        color=Color(column_1, title=title_fix(f"Legend")),
+        tooltip=Tooltip([column_2, column_1, f"count({column_1})"])
+    ).properties(
+        width=480,
+        height=400,
+        padding=24,
+    ).configure(
+        legend={"padding": 24},
+        title={"fontSize": 20, "offset": 24},
+        view={"stroke": "#FFF"},
+    )
+
+
+def topic_chart(df, column_1, column_2):
+    return Chart(
+        df,
+        title=f"{title_fix(column_1)}",
+    ).mark_bar().encode(
+        x=X(column_1, title=title_fix(column_1), sort="-y"),
+        y=Y(f"count({column_1})"),
+        color=Color(column_2, title='Count'),
+        tooltip=Tooltip([column_2, column_1, f"count({column_1})"])
+    ).properties(
+        width=480,
+        height=400,
+        padding=24,
+    ).configure(
+        legend={"padding": 24},
+        title={"fontSize": 20, "offset": 24},
+        view={"stroke": "#FFF"},
+    )
+
+
+def activity_chart(df, column_1, column_2):
+    return Chart(
+        df,
+        title=f"{title_fix(column_1)}",
+    ).mark_bar().encode(
+        x=X(column_1, title=title_fix(column_1), sort="-y"),
+        y=Y(f"count({column_1})"),
+        color=Color(column_2, title=title_fix(column_2)),
+        tooltip=Tooltip([column_2, column_1, f"count({column_1})"])
+    ).properties(
+        width=480,
+        height=400,
+        padding=24,
+    ).configure(
+        legend={"padding": 24},
+        title={"fontSize": 20, "offset": 24},
+        view={"stroke": "#FFF"},
+    )
