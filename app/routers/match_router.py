8ff0721adbbc0f42eba5e2bc5caa706f907d57fa
Jeff Desch <103085443+JeffDesch@users.noreply.github.com>
2022-10-11 22:02:16 -0500

    -Fixed some copy/paste errors -renamed some variables for clarity -switched to using class attributes instead of dict conversion

--- app/routers/match_router.py
+++ app/routers/match_router.py
@@ -15,11 +15,10 @@ async def create_match(data: MatchUpdate):
     """Insert a new mentor/mentee pairing into the collection
     <pre><code>
     @param data: JSON[MatchUpdate]
-    @return JSON[Boolean] - success or failure of update</pre></code>"""
-    data = data.dict()
+    @return JSON[Boolean] - success or failure of match creation</pre></code>"""
     return {"result": Router.db.upsert_to_set_array("Matches",
-                                                    {"mentor_id": data["mentor_id"]},
-                                                    {"mentee_ids": data["mentee_id"]})}
+                                                    {"mentor_id": data.mentor_id},
+                                                    {"mentee_ids": data.mentee_id})}
 
 
 @Router.post("/delete/match")
@@ -27,28 +26,26 @@ async def delete_match(data: MatchUpdate):
     """Remove a mentor/mentee pairing from the collection
     <pre><code>
     @param data: JSON[MatchUpdate]
-    @return JSON[Boolean] - success or failure of update</pre></code>"""
-    data = data.dict()
+    @return JSON[Boolean] - success or failure of match removal</pre></code>"""
     return {"result": Router.db.delete_from_array("Matches",
-                                                  {"mentor_id": data["mentor_id"]},
-                                                  {"mentee_ids": data["mentee_id"]})}
+                                                  {"mentor_id": data.mentor_id},
+                                                  {"mentee_ids": data.mentee_id})}
 
 
 @Router.post("/read/match")
-async def get_match(query: MatchQuery):
+async def get_match(data: MatchQuery):
     """Retrieve all matching mentor/mentee objects for a given mentee/mentor
     <pre><code>
-    @param query: JSON[MatchQuery]
+    @param data: JSON[MatchQuery]
     @return JSON[Array[Mentor]] | JSON[Array[Mentee]]</pre></code>"""
-    query = query.dict()
-    if query["user_type"] == "mentor":
-        db_col = "Mentees"
-        query = {"profile_id": {"$in": Router.db.first("Matches", {"mentor_id": query["user_id"]})['mentee_ids']}}
-    elif query["user_type"] == "mentee":
-        db_col = "Mentors"
-        query = {"profile_id": {
-            "$in": [mentor["mentor_id"] for mentor in Router.db.read("Matches", {"mentee_ids": query["user_id"]})]}}
+    if data.user_type == "mentor":
+        collection = "Mentees"
+        user_query = {"profile_id": {"$in": Router.db.first("Matches", {"mentor_id": data.user_id})['mentee_ids']}}
+    elif data.user_type == "mentee":
+        collection = "Mentors"
+        user_query = {"profile_id": {
+            "$in": [mentor["mentor_id"] for mentor in Router.db.read("Matches", {"mentee_ids": data.user_id})]}}
     else:
         raise ValueError("get_match: user_type must be 'mentor' or 'mentee'")
 
-    return {"result": Router.db.read(db_col, query)}
+    return {"result": Router.db.read(collection, user_query)}
