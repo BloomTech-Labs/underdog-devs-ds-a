e0b29c245d72004494df4bf9f76d5c42eebefeb4
Jeff Desch <103085443+JeffDesch@users.noreply.github.com>
2022-11-09 14:24:21 -0600

    - Fixed failing test case

--- tests/unit_tests/test_app_graphs.py
+++ tests/unit_tests/test_app_graphs.py
@@ -6,7 +6,7 @@ from app.graphs import title_fix
 class TestGraphs(unittest.TestCase):
 
     def test_title_fix_output(self):
-        test_title, test_title_fixed = "_This__is_text_!_", "This Is Text!"
+        test_title, test_title_fixed = "This_is_text!", "This Is Text!"
 
         self.assertEqual(title_fix(test_title), test_title_fixed)
         self.assertEqual(title_fix(test_title_fixed), test_title_fixed)
