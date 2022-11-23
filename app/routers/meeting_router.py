0d6fd559dff3a2734578f44a0aee0ef6c63e1dc1
Gregory <setantaontherock1@gmail.com>
2022-11-23 18:19:59 +0100

    Add meeting endpoints in meeting_router.py

---
+++ app/routers/meeting_router.py
@@ -0,0 +1,56 @@
+from typing import Optional, Dict
+
+from fastapi import APIRouter, HTTPException
+from pymongo.errors import DuplicateKeyError
+
+from app.data import MongoDB
+from app.schema import Meeting, MeetingUpdate
+
+Router = APIRouter(
+    tags=["Meeting Operations"],
+)
+
+Router.db = MongoDB()
+
+
+@Router.post("/create/meeting")
+async def create_meeting(data: Meeting):
+    """Creates a meeting
+    <pre><code>
+    @param data: JSON[Meeting]
+    @return JSON[Boolean] - Indicates success or failure of document creation</pre></code>"""
+    try:
+        return {"result": Router.db.create("Meetings", data.dict())}
+    except DuplicateKeyError:
+        raise HTTPException(
+            status_code=409, detail="Meeting ID must be unique.")
+
+
+@Router.get("/read/meeting/{profile_id}")
+async def read_meeting(data: Optional[Dict] = None):
+    """Displays meeting(s) that meet provided criteria. Displays all meetings if no input provided
+    <pre><code>
+    @param data: JSON[Optional[Dict]]
+    @return JSON[Array[Meeting]]</pre></code>"""
+    return {"result": Router.db.read("Meetings", data)}
+
+
+@Router.put("/update/meeting/{meeting_id}")
+async def update_meeting(meeting_id: str, update_data: MeetingUpdate):
+    """Updates a meeting
+    <pre><code>
+    @param meeting_id: str
+    @param update_data: JSON[MeetingUpdate]
+    @return JSON[Boolean] - Indicates success or failure of update</pre></code>"""
+    data = update_data.dict(exclude_none=True)
+    return {"result": Router.db.update("Meeting", {"meeting_id": meeting_id}, data)}
+
+
+@Router.delete("/delete/meeting")
+async def delete_meeting(meeting_id: str):
+    """Deletes meeting
+    <pre><code>
+    @param meeting_id: str
+    @return JSON[JSON[meeting_id]]</pre></code>"""
+    Router.db.delete("Meeting", {"meeting_id": meeting_id})
+    return {"result": {"deleted": meeting_id}}
