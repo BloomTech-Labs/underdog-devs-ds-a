e58d84afed01f1d649b657a663c693348b7435dd
Feliciano-S <100312796+Feliciano-S@users.noreply.github.com>
2023-01-25 14:14:30 -0500

    updated endpoints to correlate to the newly created classes in model.py file

--- app/routers/model_router.py
+++ app/routers/model_router.py
@@ -3,21 +3,32 @@ from typing import Optional
 from fastapi import APIRouter
 
 from app.data import MongoDB
-from app.model import MatcherSortSearch
+from app.model import MenteeMatcherSearch
+from app.model import MentorMatcherSearch
 
 Router = APIRouter(
     tags=["Model Operations"],
 )
 Router.db = MongoDB()
-Router.matcher = MatcherSortSearch()
+Router.mentee_matcher = MenteeMatcherSearch()
+Router.mentor_matcher = MentorMatcherSearch()
 
-
-@Router.post("/match/{profile_id}")
-async def match(profile_id: str, n_matches: Optional[int] = None):
-    """Returns n_matches mentor profiles for mentee profile_id
+@Router.post("/mentee_match/{profile_id}")
+async def mentee_match(profile_id: str, n_matches: Optional[int] = None):
+    """Returns mentor profiles for mentee profile_id
     <pre><code>
     @param profile_id: str
     @param n_matches: Optional[int] = None
     @return JSON[Array[profile_id]]</pre></code>
     """
-    return {"result": Router.matcher(profile_id, n_matches)}
+    return {"result": Router.mentee_matcher(profile_id, n_matches)}
+
+@Router.post("/mentor_match/{profile_id}")
+async def mentor_match(profile_id: str, n_matches: Optional[int] = None):
+    """Returns mentee profiles for mentor profile_id
+    <pre><code>
+    @param profile_id: str
+    @param n_matches: Optional[int] = None
+    @return JSON[Array[profile_id]]</pre></code>
+    """
+    return {"result": Router.mentor_matcher(profile_id, n_matches)}
