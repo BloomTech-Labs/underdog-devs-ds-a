c47f5688042bc20de1d4d5c38edf5a1c10a1b0f8
BrokenShell <webmaster@sharpdesigndigital.com>
2023-02-15 12:05:33 -0800

    adds matches/all/obj to read all matches as objects.

--- app/routers/model_router.py
+++ app/routers/model_router.py
@@ -11,6 +11,7 @@ Router.db = MongoDB()
 Router.mentee_matcher = MenteeMatcherSearch()
 Router.mentor_matcher = MentorMatcherSearch()
 
+
 @Router.post("/mentee-match/{profile_id}")
 async def mentee_match(profile_id: str, n_matches: Optional[int] = None):
     """Returns mentor profiles for mentee profile_id
@@ -20,6 +21,7 @@ async def mentee_match(profile_id: str, n_matches: Optional[int] = None):
     @return JSON[Array[profile_id]]</pre></code>
     """
     return {"result": Router.mentee_matcher(profile_id, n_matches)}
+
 
 @Router.post("/mentor-match/{profile_id}")
 async def mentor_match(profile_id: str, n_matches: Optional[int] = None):
